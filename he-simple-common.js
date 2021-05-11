25c15a86efa178bc17ccdfc9ea665ef36ee8af09
qianxingqianzui <72236630+qianxingqianzui@users.noreply.github.com>
2021-05-11 11:48:45 +0800

    Update he-simple-common.js

--- he-simple-common.js
+++ he-simple-common.js
@@ -1,9 +1,9 @@
 (function (d) {
   var c = d.createElement('link')
   c.rel = 'stylesheet'
-  c.href = 'https://widget.heweather.net/simple/static/css/he-simple.css?v=1.4.0'
+  c.href = 'https://cdn.jsdelivr.net/gh/qianxingqianzui/tuchuang@main/he-simple.css?v=1.4.0'
   var s = d.createElement('script')
-  s.src = 'https://widget.heweather.net/simple/static/js/he-simple.js?v=1.4.0'
+  s.src = 'https://cdn.jsdelivr.net/gh/qianxingqianzui/tuchuang@main/he-simple.js?v=1.4.0'
   var sn = d.getElementsByTagName('script')[0]
   sn.parentNode.insertBefore(c, sn)
   sn.parentNode.insertBefore(s, sn)
